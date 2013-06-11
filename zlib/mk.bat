a07c6cbf6c1d6d1079102275c657e342b0386f73
MITSUNARI Shigeo <herumi@nifty.com>
2013-06-11 11:31:46 +0900

    update zlib 1.2.8

--- zlib/mk.bat
+++ zlib/mk.bat
@@ -1,7 +1,10 @@
 rem @echo off
 set PATH=..\tool;%PATH%
-set VAR=1.2.5
-bzip2 -dc zlib-%VAR%.tar.bz2 | tar xvf -
+set VAR=1.2.8
+tar xvf zlib-%VAR%.tar.gz
+
+md include
+md lib
 
 pushd zlib-%VAR%
 
@@ -9,19 +12,21 @@ if /i "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
 	pushd contrib\masmx64
 	call bld_ml64
 	popd
-	nmake -f win32/Makefile.msc AS=ml64 LOC="-DASMV -DASMINF -I./" OBJA="inffasx64.obj gvmat64.obj inffas8664.obj"
+	nmake -f ..\Makefile.msc MTMD=/MD AS=ml64 LOC="-DASMV -DASMINF -I./" OBJA="inffasx64.obj gvmat64.obj inffas8664.obj"
+	copy zlib_md.lib ..\lib
+	nmake -f ..\Makefile.msc clean
+	nmake -f ..\Makefile.msc MTMD=/MT AS=ml64 LOC="-DASMV -DASMINF -I./" OBJA="inffasx64.obj gvmat64.obj inffas8664.obj"
+	copy zlib_mt.lib ..\lib
 ) else (
-	nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF" OBJA="inffas32.obj match686.obj"
+	nmake -f ..\Makefile.msc MTMD=/MD LOC="-DASMV -DASMINF" OBJA="inffas32.obj match686.obj"
+	copy zlib_md.lib ..\lib
+	nmake -f ..\Makefile.msc clean
+	nmake -f ..\Makefile.msc MTMD=/MT LOC="-DASMV -DASMINF" OBJA="inffas32.obj match686.obj"
+	copy zlib_mt.lib ..\lib
 )
 
 popd
-md include
-md lib
-rem md dll
 pushd zlib-%VAR%
 copy zlib.h ..\include
 copy zconf.h ..\include
-copy zlib.lib ..\lib
-rem copy zdll.lib ..\lib
-rem copy zlib1.dll ..\dll
 popd
